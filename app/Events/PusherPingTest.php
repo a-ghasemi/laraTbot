eee1409bb172bca88cd9e67a30dbc7a7f6a30b9e
Armin Ghasemi <datarivers.org@gmail.com>
2022-12-21 23:09:42 +0100

    adds pusher ping

---
+++ app/Events/PusherPingTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Str;
+
+class PusherPingTest implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $randomString;
+
+    public function __construct()
+    {
+        $this->randomString = Str::random(10);
+        //
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('my-channel');
+    }
+
+    public function broadcastAs()
+    {
+        return 'my-event';
+    }
+}
